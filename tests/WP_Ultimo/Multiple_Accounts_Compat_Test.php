4ae00895685604ad07eb5127f0d9f757c9d5fc94
David Stone <david@nnucomputerwhiz.com>
2026-06-18 19:11:25 -0600

    fix: qualify multiple accounts compat test class (#1462)

--- tests/WP_Ultimo/Multiple_Accounts_Compat_Test.php
+++ tests/WP_Ultimo/Multiple_Accounts_Compat_Test.php
@@ -22,7 +22,7 @@ class Multiple_Accounts_Compat_Test extends WP_UnitTestCase {
 	 */
 	public function test_add_column_content_returns_empty_string_for_null_unrelated_column(): void {
 
-		$output = Multiple_Accounts_Compat::get_instance()->add_column_content(null, 'bbp_user_role', 123);
+		$output = \WP_Ultimo\Compat\Multiple_Accounts_Compat::get_instance()->add_column_content(null, 'bbp_user_role', 123);
 
 		$this->assertSame('', $output);
 	}
@@ -32,7 +32,7 @@ class Multiple_Accounts_Compat_Test extends WP_UnitTestCase {
 	 */
 	public function test_add_column_content_preserves_unrelated_column_output(): void {
 
-		$output = Multiple_Accounts_Compat::get_instance()->add_column_content('Subscriber', 'role', 123);
+		$output = \WP_Ultimo\Compat\Multiple_Accounts_Compat::get_instance()->add_column_content('Subscriber', 'role', 123);
 
 		$this->assertSame('Subscriber', $output);
 	}
