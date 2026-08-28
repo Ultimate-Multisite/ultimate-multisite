b96df59c18d6526dbec4384a8bfeb22c641f50f7
David Stone <david@nnucomputerwhiz.com>
2026-08-28 16:08:19 -0600

    fix(sso): disable SSO by default (#1741)
    * fix(sso): disable SSO by default
    
    * fix(sso): normalize enabled setting

--- inc/class-settings.php
+++ inc/class-settings.php
@@ -2202,7 +2202,7 @@ class Settings implements \WP_Ultimo\Interfaces\Singleton {
 			'domain_mapping_instructions'        => '',
 
 			// SSO (registered via hooks)
-			'enable_sso'                         => 1,
+			'enable_sso'                         => 0,
 
 			// Other
 			'hide_tours'                         => 0,
