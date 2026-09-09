835b210b27f01ae06055fc3638de8fb6e36b5089
David Stone <david@nnucomputerwhiz.com>
2026-09-09 15:49:41 -0600

    fix(i18n): normalize translator comments (#1802)

--- inc/integrations/host-providers/class-base-host-provider.php
+++ inc/integrations/host-providers/class-base-host-provider.php
@@ -288,7 +288,7 @@ abstract class Base_Host_Provider implements DNS_Provider_Interface {
 
 		$actions = [
 			'activate' => [
-				// translators: %s is the integration name
+				// translators: %s is the integration name.
 				'title' => sprintf(__('Setup %s', 'ultimate-multisite'), $this->get_title()),
 				'url'   => wu_network_admin_url(
 					'wp-ultimo-hosting-integration-wizard',
@@ -574,7 +574,7 @@ abstract class Base_Host_Provider implements DNS_Provider_Interface {
 
 		$explainer_lines = [
 			'will'     => [
-				// translators: %s is the name of the integration e.g. RunCloud
+				// translators: %s: hosting provider name.
 				'send_domains' => sprintf(__('Send API calls to %s servers with domain names added to this network', 'ultimate-multisite'), $this->get_title()),
 			],
 			'will_not' => [],
@@ -582,11 +582,11 @@ abstract class Base_Host_Provider implements DNS_Provider_Interface {
 
 		if ($this->supports('autossl')) {
 
-			// translators: %s is the name of the integration e.g. RunCloud
+			// translators: %s: hosting provider name.
 			$explainer_lines['will'][] = sprintf(__('Fetch and install a SSL certificate on %s platform after the domain is added.', 'ultimate-multisite'), $this->get_title());
 		} else {
 
-			// translators: %s is the name of the integration e.g. RunCloud
+			// translators: %s: hosting provider name.
 			$explainer_lines['will_not'][] = sprintf(__('Fetch and install a SSL certificate on %s platform after the domain is added. This needs to be done manually.', 'ultimate-multisite'), $this->get_title());
 		}
 
